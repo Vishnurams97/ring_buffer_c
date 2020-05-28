8fd9ebee215e841a20002e7e5885e2576bf374ef
Vishnurams97 <34983241+Vishnurams97@users.noreply.github.com>
2020-05-28 14:16:09 +0530

    Update ring_buffer.c

--- ring_buffer.c
+++ ring_buffer.c
@@ -1,17 +1,15 @@
 
-#include "ring_buffer.h"
+#include "ring_buffer_lib.h"
 #include <stdlib.h>
 #include <stdio.h> 
 #include <string.h>
 
-ring_buffer_t * init_cbuffer(int length){
-    ring_buffer_t * p_buffer_t;
+void init_cbuffer(int length,  ring_buffer_t * p_buffer_t){
     p_buffer_t->head = 0;
     p_buffer_t->tail = 0;
-    p_buffer_t->total_slots = length - 1;
+    p_buffer_t->total_slots = length;
     p_buffer_t->is_full = false;
-    p_buffer_t->packets_ptr = (storage_structure *)calloc(length, sizeof(storage_structure));
-    return p_buffer_t;
+    p_buffer_t->packets_ptr = (storage_structure *)malloc(length * sizeof(storage_structure));
 }
 
 bool is_buffer_full(ring_buffer_t * p_buffer_t){
@@ -23,12 +21,13 @@ bool is_buffer_empty(ring_buffer_t * p_buffer_t){
 }
 
 ret_code_t uninit_cbuffer(ring_buffer_t * p_buffer_t){
-    free(p_buffer_t);
+    free(p_buffer_t->packets_ptr);
     return BFR_SUCCESS;
 }
 
 ret_code_t write_item(ring_buffer_t * p_buffer_t, storage_structure * p_data_t){
-    memcpy(&p_buffer_t->packets_ptr[p_buffer_t->head], p_data_t, sizeof(storage_structure));
+    
+    p_buffer_t->packets_ptr[p_buffer_t->head] = *p_data_t;
     p_buffer_t->head = (p_buffer_t->head + 1) % p_buffer_t->total_slots;
     if(!is_buffer_full(p_buffer_t)){
         if(p_buffer_t->head == p_buffer_t->tail){
@@ -42,7 +41,10 @@ ret_code_t write_item(ring_buffer_t * p_buffer_t, storage_structure * p_data_t){
 }
 
 storage_structure * read_head_item(ring_buffer_t * p_buffer_t){
-    return  &(p_buffer_t->packets_ptr[p_buffer_t->head - 1]);
+    if(p_buffer_t->head-1 < 0){
+        p_buffer_t->head += p_buffer_t->total_slots;
+    }
+    return  &p_buffer_t->packets_ptr[p_buffer_t->head - 1];
 }
 
 storage_structure * read_tail_item(ring_buffer_t * p_buffer_t){
@@ -56,11 +58,9 @@ ret_code_t remove_head_item(ring_buffer_t * p_buffer_t){
     else{
         if(is_buffer_full(p_buffer_t)){
             p_buffer_t->is_full = false;
-            p_buffer_t->head-=1;
-        }   
-        else{
-            p_buffer_t->head-=1;
         }
+        p_buffer_t->head = p_buffer_t->head - 1;
+        if(p_buffer_t->head < 0){p_buffer_t->head += p_buffer_t->total_slots;}
     }
 }
 
@@ -84,9 +84,13 @@ ret_code_t reset_cbuffer(ring_buffer_t * p_buffer_t){
     p_buffer_t->tail = 0;
     p_buffer_t->total_slots = 0;
     p_buffer_t->is_full = false;
-    free(p_buffer_t->packets_ptr); 
+    free(p_buffer_t->packets_ptr);
 }
 
 int get_queue_capacity(ring_buffer_t * p_buffer_t){
     return p_buffer_t->total_slots;
+}
+
+int get_queue_mems_length(ring_buffer_t * p_buffer_t){
+    return p_buffer_t->is_full ? p_buffer_t->total_slots: p_buffer_t->head - p_buffer_t->tail ;
 }
